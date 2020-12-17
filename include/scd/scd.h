cc32c7beed90b8da9a73239d2a500ea35ff5ac03
guthriejd1 <guthriejd1@gmail.com>
2020-12-17 09:58:41 -0800

    Added 2D collision detection.

--- include/scd/scd.h
+++ include/scd/scd.h
@@ -5,6 +5,20 @@
 #include <cassert>
 
 namespace scd{
+    struct Superquadric2{
+        Superquadric2(double m_ax, double m_ay, double m_e) : ax(m_ax), ay(m_ay), e(m_e){
+            assert( (m_ax > 0) && (m_ay > 0) );
+            assert( (m_e >= 0) && (m_e <= 2) );
+        }
+        Superquadric2() = default;
+
+        double ax;
+        double ay;
+        double e;
+        Eigen::Transform<double,2,Eigen::Isometry> X;
+    };
+
+
     struct Ellipsoid3 {
         Ellipsoid3(double m_ax, double m_ay, double m_az) : ax(m_ax), ay(m_ay), az(m_az) {
             assert((m_ax > 0) && (m_ay > 0) && (m_az > 0));
@@ -22,6 +36,8 @@ namespace scd{
             assert( (m_e1 >= 0) && (m_e1 <= 2) );
             assert( (m_e2 >= 0) && (m_e2 <= 2) );
         }
+        Superquadric3() = default;
+
         double ax;
         double ay;
         double az;
@@ -41,7 +57,16 @@ namespace scd{
         }
         ceres::Solver::Options ceres_options;
     };
-
+    struct Request2{
+        Request2(){
+            // Default Ceres solver options for scd
+            ceres_options.minimizer_type = ceres::TRUST_REGION;
+            ceres_options.max_num_iterations = 100;
+            ceres_options.linear_solver_type = ceres::DENSE_QR;
+            ceres_options.minimizer_progress_to_stdout = false;
+        }
+        ceres::Solver::Options ceres_options;
+    };
     struct Result{
         bool collide;
         double omega;
@@ -51,6 +76,14 @@ namespace scd{
         ceres::Solver::Summary ceres_summary;
     };
 
-    void collide(const Superquadric3& SQ1, const Ellipsoid3& E2, const Request& request, Result& result);
+    struct Result2{
+        bool collide;
+        double theta;
+        Superquadric2 Ec;
+        Eigen::Matrix<double,2,1> x_eb;
+        ceres::Solver::Summary ceres_summary;
+    };
 
+    void collide(const Superquadric3& SQ1, const Ellipsoid3& E2, const Request& request, Result& result);
+    void collide2(const Superquadric2& SQ1, const Superquadric2& E2, const Request2& request, Result2& result);
 } //namespace scd

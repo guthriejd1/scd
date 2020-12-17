4ab8916a46e1b7f67d465ed18a6bdbd4b6aa9e01
guthriejd1 <guthriejd1@gmail.com>
2020-12-17 09:55:10 -0800

    Added 2D collision detection.

---
+++ test/data_utilities.h
@@ -0,0 +1,125 @@
+# pragma once
+
+namespace scd {
+    namespace test {
+// Code based on
+// https://stackoverflow.com/questions/34247057/how-to-read-csv-file-and-assign-to-eigen-matrix
+
+        template<typename M>
+        M ReadCsvMatrix(const std::string &path, uint line_start) {
+            std::ifstream indata;
+            indata.open(path);
+            if (indata.fail())
+                std::cerr << "Failed to open file at " << path << std::endl;
+            std::string line;
+            std::vector<double> values;
+            uint line_n = 0;
+            uint line_end = line_start + M::RowsAtCompileTime - 1;
+            while ((std::getline(indata, line)) && (line_n <= line_end)) {
+                if (line_n >= line_start) {
+                    std::stringstream lineStream(line);
+                    std::string cell;
+                    while (std::getline(lineStream, cell, ',')) {
+                        values.push_back(std::stod(cell));
+                    }
+                }
+                ++line_n;
+            }
+
+            return Eigen::Map<const Eigen::Matrix<typename M::Scalar, M::RowsAtCompileTime, M::ColsAtCompileTime,
+                    Eigen::RowMajor>>(values.data(), M::RowsAtCompileTime, M::ColsAtCompileTime);
+        }
+
+// TODO (jay) Combine with read_csv_matrix(). Currently separate because RowMajor not allowed for column vectors.
+        template<typename M>
+        M ReadCsvVector(const std::string &path, uint line_start) {
+            std::ifstream indata;
+            indata.open(path);
+            if (indata.fail())
+                std::cerr << "Failed to open file at " << path << std::endl;
+            std::string line;
+            std::vector<double> values;
+            uint line_n = 0;
+            uint line_end = line_start + M::RowsAtCompileTime - 1;
+            while ((std::getline(indata, line)) && (line_n <= line_end)) {
+                if (line_n >= line_start) {
+                    std::stringstream lineStream(line);
+                    std::string cell;
+                    while (std::getline(lineStream, cell, ',')) {
+                        values.push_back(std::stod(cell));
+                    }
+                }
+                ++line_n;
+            }
+
+            return Eigen::Map<const Eigen::Matrix<typename M::Scalar, M::RowsAtCompileTime, M::ColsAtCompileTime,
+                    Eigen::ColMajor>>(values.data(), M::RowsAtCompileTime, M::ColsAtCompileTime);
+        }
+
+// TODO (jay) Refactor this using templates instead of overloading
+        void ReadShapeFile(std::string file_path, uint n_line, Superquadric3& SQ){
+            const uint n = 3;
+            std::ifstream indata;
+            indata.open(file_path);
+
+            Eigen::Matrix<double,1,n> a = scd::test::ReadCsvMatrix<Eigen::Matrix<double,1,n>>(file_path, n_line);
+            Eigen::Matrix<double,1,n-1> e = scd::test::ReadCsvMatrix<Eigen::Matrix<double,1,n-1>>(file_path, n_line+1);
+            Eigen::Matrix<double,n,n> R = scd::test::ReadCsvMatrix<Eigen::Matrix<double,n,n>>(file_path, n_line+2);
+            Eigen::Matrix<double,n,1> t = scd::test::ReadCsvVector<Eigen::Matrix<double,n,1>>(file_path, n_line+5);
+
+            SQ.ax = a(0);
+            SQ.ay = a(1);
+            SQ.az = a(2);
+            SQ.e1 = e(0);
+            SQ.e2 = e(1);
+
+            Eigen::Matrix<double,4,4> X;
+            X.setIdentity();
+            X.block<3,3>(0,0) = R;
+            X.block<3,1>(0,3) = t;
+            SQ.X.matrix() =X;
+            return;
+        }
+        void ReadShapeFile(std::string file_path, uint n_line, Ellipsoid3& SQ){
+            const uint n = 3;
+            std::ifstream indata;
+            indata.open(file_path);
+            if (indata.fail())
+                std::cerr << "Failed to open file " << file_path << std::endl;
+            Eigen::Matrix<double,1,n> a = scd::test::ReadCsvMatrix<Eigen::Matrix<double,1,n>>(file_path, n_line);
+            Eigen::Matrix<double,1,n-1> e = scd::test::ReadCsvMatrix<Eigen::Matrix<double,1,n-1>>(file_path, n_line+1);
+            Eigen::Matrix<double,n,n> R = scd::test::ReadCsvMatrix<Eigen::Matrix<double,n,n>>(file_path, n_line+2);
+            Eigen::Matrix<double,n,1> t = scd::test::ReadCsvVector<Eigen::Matrix<double,n,1>>(file_path, n_line+5);
+
+            SQ.ax = a(0);
+            SQ.ay = a(1);
+            SQ.az = a(2);
+
+            Eigen::Matrix<double,4,4> X;
+            X.setIdentity();
+            X.block<3,3>(0,0) = R;
+            X.block<3,1>(0,3) = t;
+            SQ.X.matrix() = X;
+            return;
+        }
+
+
+        void ReadTestFile(std::string file_path, uint n_line, Superquadric3& SQ1, Ellipsoid3& E2, Ellipsoid3& E2c, Result& result){
+            std::ifstream indata;
+            indata.open(file_path);
+            if (indata.fail())
+                std::cerr << "Failed to open file " << file_path << std::endl;
+
+            const uint n = 3;
+            ReadShapeFile(file_path, n_line, SQ1);
+            ReadShapeFile(file_path, n_line+8, E2);
+            ReadShapeFile(file_path, n_line+16, E2c);
+            Eigen::Matrix<double,1,3> d = scd::test::ReadCsvMatrix<Eigen::Matrix<double,1,3>>(file_path, n_line+24);
+            result.eta = d(0);
+            result.omega = d(1);
+            result.collide = static_cast<bool>(d(2));
+            return;
+        }
+
+    } // namespace test
+} // namespace scd
--- test/scd_test.cpp
+++ test/scd_test.cpp
@@ -1,7 +1,73 @@
 #include <gtest/gtest.h>
-#include <cmath>>
+#include <cmath>
+#include <fstream>
 #include "../include/scd/scd.h"
+#include "data_utilities.h"
+#include <iostream>
 
+GTEST_TEST(Test2D, CreateShape){
+    Eigen::Rotation2D<double> Xr(-M_PI/5);
+    Eigen::Translation<double,2> Xt(1,-1);
+    Eigen::Transform<double,2,Eigen::Isometry> X = Xt*Xr;
+    scd::Superquadric2 SQ1(1,2,0.25);
+    SQ1.X = X;
+
+    Xr.angle() = -M_PI/3;
+    Xt.x() = -3;
+    Xt.y() = 3;
+    X = Xt*Xr;
+    scd::Superquadric2 E2(0.5,2.0,1.0);
+    E2.X = X;
+
+    scd::Request2 request;
+    scd::Result2 result;
+    scd::collide2(SQ1, E2, request, result);
+
+    Eigen::Transform<double,2,Eigen::Isometry> X_expect;
+    X_expect.matrix() <<  0.5000,    0.8660,   -0.3587,
+                         -0.8660,    0.5000,    0.3587,
+                          0,         0,         1.0000;
+
+    ASSERT_LT(( X_expect.matrix() - result.Ec.X.matrix() ).norm(), 1E-4);
+    EXPECT_NEAR(result.theta,2.726233503871522, 1E-6);
+    ASSERT_FALSE(result.collide);
+}
+GTEST_TEST(TestCollision, Data){
+    std::string file_path = "../test/data/cube_ellipsoid.txt";
+    scd::Superquadric3 SQ1;
+    scd::Ellipsoid3 E2;
+    scd::Ellipsoid3 E2c_expect;
+    scd::Result result_expect;
+    uint n_line = 0;
+    scd::test::ReadTestFile(file_path, n_line, SQ1, E2, E2c_expect, result_expect);
+
+    scd::Request request;
+    scd::Result result;
+    scd::collide(SQ1, E2, request, result);
+
+    ASSERT_EQ(result.collide, result_expect.collide);
+    EXPECT_NEAR(result.eta, result_expect.eta, 1E-6);
+    EXPECT_NEAR(result.omega, result_expect.omega, 1E-6);
+}
+GTEST_TEST(TestCollision, EllipsoidEllipsoidBatch){
+    std::string file_path = "../test/data/ellipsoid_ellipsoid_batch.txt";
+    uint n_line = 0;
+    std::cout << "Begin batch test...\n";
+    for (int n_test = 0; n_test < 1000; n_test++) {
+        scd::Superquadric3 SQ1;
+        scd::Ellipsoid3 E2;
+        scd::Ellipsoid3 E2c_expect;
+        scd::Result result_expect;
+        scd::test::ReadTestFile(file_path, n_line, SQ1, E2, E2c_expect, result_expect);
+        scd::Request request;
+        scd::Result result;
+        scd::collide(SQ1, E2, request, result);
+
+        ASSERT_EQ(result.collide, result_expect.collide);
+        // TODO (jay) Add angle wrapping to allow comparison
+        n_line += 25;
+    }
+}
 
 GTEST_TEST(TestCollision, CubeEllipsoid){
     scd::Superquadric3 SQ1(1.0,2.0,3.0,0.25,0.25);
@@ -21,16 +87,18 @@ GTEST_TEST(TestCollision, CubeEllipsoid){
     scd::Result result;
     scd::collide(SQ1, E2, request, result);
 
-    // TODO(jay) Load validation data from file to allow more numerical precision
     Eigen::Transform<double,3,Eigen::Isometry> X;
     X.matrix() <<      0.1545,    0.4755,   -0.8660,    2.9909,
                       -0.9574,   -0.1442,   -0.2500,    1.7982,
                       -0.2438,    0.8678,    0.4330,    3.5964,
                        0,         0,         0,         1.0000;
 
+    // Data was copied from MATLAB printout, keeping only 4 decimals. Tolerances reflect this.
     ASSERT_LT(( X.matrix() - result.Ec.X.matrix() ).norm(), 0.0001);
     EXPECT_NEAR(result.eta, 1.2450, 1E-4);
     EXPECT_NEAR(result.omega, 3.0920, 1E-4);
+
+
 }
 int main(int argc, char* argv[])
 {
